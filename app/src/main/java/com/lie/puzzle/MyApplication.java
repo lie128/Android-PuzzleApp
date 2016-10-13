9e4c61124e26473f74dbc2e6a2b4eb9f667f3805
cigar0223 <415718252@qq.com>
2016-10-13 16:26:11 +0800

    项目初始化

---
+++ app/src/main/java/com/lie/puzzle/MyApplication.java
@@ -0,0 +1,13 @@
+package com.lie.puzzle;
+
+import android.app.Application;
+
+/**
+ * Created by hsj on 2016/10/13.
+ */
+public class MyApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
---
+++ app/src/main/java/com/lie/puzzle/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.lie.puzzle.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.lie.puzzle.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
